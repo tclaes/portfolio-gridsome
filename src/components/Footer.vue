a937a8fac953705fc0184586c29f20d2985aa389
Tom Claes <tom.claes@cegeka.com>
2020-04-28 16:29:10 +0200

    footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="footer">
+    <div></div>
+    <div></div>
+    <div class="copyright" v-html="copy">{{ copy }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      const year = new Date().getFullYear();
+
+      return {
+        copy: `&copy; ${year}`
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+#footer {
+  background: #333;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  min-height: 30px;
+  padding: 0 20px;
+}
+
+</style>
